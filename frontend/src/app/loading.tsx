2177965c61286347c729564fa3bea18f935d4ca2
mezger75 <9771804@gmail.com>
2023-11-28 18:12:06 -0300

    feat: add loading.tsx page

---
+++ frontend/src/app/loading.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const LoadingPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen py-2">
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default LoadingPage;
