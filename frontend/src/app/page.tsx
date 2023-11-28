343465f2c73ac52fa631510131b710e96ebe7cc7
mezger75 <9771804@gmail.com>
2023-11-28 18:15:13 -0300

    feat: add getting session id logic by clicking the button

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,20 @@
 "use client";
+import { POST } from "./api/route";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
+  const router = useRouter();
+  async function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
-    console.log("The button was clicked.");
+    const response = await POST();
+    const data = await response.json();
+    console.log(data);
+
+    if (data) {
+      router.push(`/game/${data.id}`);
+    }
   }
+
   return (
     <main>
       <div className="flex items-center justify-center min-h-screen py-2">
@@ -12,7 +22,7 @@ export default function Home() {
           className="bg-gray-600 hover:bg-gray-500 text-gray-100 font-bold py-2 px-4 rounded transform active:translate-y-0.5"
           onClick={handleClick}
         >
-          Start your journey here
+          Retype Me
         </button>
       </div>
     </main>
