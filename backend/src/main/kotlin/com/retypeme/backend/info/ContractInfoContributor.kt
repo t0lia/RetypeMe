b99df4f623384a0c850e9bc643b7d1b6438bd109
apozdniakov <t0lia.gm@gmail.com>
2024-01-14 23:32:55 +0100

    add contract on backend

---
+++ backend/src/main/kotlin/com/retypeme/backend/info/ContractInfoContributor.kt
@@ -0,0 +1,23 @@
+package com.retypeme.backend.info
+
+import com.retypeme.backend.service.SmartContractService
+import org.springframework.boot.actuate.info.Info
+import org.springframework.boot.actuate.info.InfoContributor
+import org.springframework.stereotype.Component
+
+@Component
+class ContractInfoContributor(private val smartContractService: SmartContractService) : InfoContributor {
+
+    override fun contribute(builder: Info.Builder) {
+        builder.withDetail("smart-contract", getSmartContractInfo())
+    }
+
+    private fun getSmartContractInfo(): Map<String, Any> {
+        val infoMap = mutableMapOf<String, Any>()
+
+        infoMap["balance"] = smartContractService.getBalance()
+        infoMap["networkVersion"] = smartContractService.getNetworkVersion()
+
+        return infoMap
+    }
+}
