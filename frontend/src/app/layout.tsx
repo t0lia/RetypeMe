599e6be38d6fd3ed8de9e9bd2a58a2ad73a43de8
mezger75 <9771804@gmail.com>
2024-01-17 20:07:21 -0300

    feature add metadata keywords, favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -8,6 +8,7 @@ export const metadata: Metadata = {
   title: "RetypeMe - Learn | Play | Own",
   description:
     "Master blind typing skills, race against friends, and own your achievements as NFTs on RetypeMe. Elevate your typing experience with engaging competitions and a community-driven platform. Start your journey to faster, more accurate typing today!",
+  keywords: ["blind typing", "type racer", "monkey type", "klavogonki"],
 };
 
 export default function RootLayout({
