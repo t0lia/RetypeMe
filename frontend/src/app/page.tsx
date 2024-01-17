c0f8faf36630b4062aabc53866eb20e28c057793
mezger75 <110202847+mezger75@users.noreply.github.com>
2024-01-18 02:12:10 +0300

    feature add metadata keywords, favicon (#41)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,6 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
-import { CREATE } from "./api/route";
 import { connectWallet, formatWallet } from "./helpers";
 
 import DropDownFaucetMenu from "./components/dropdown/dropdownFaucetMenu";
