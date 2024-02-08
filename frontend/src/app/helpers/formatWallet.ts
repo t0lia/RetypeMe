b108d00d921edac84c5677d49175be29109c6faa
mezger75 <9771804@gmail.com>
2024-02-08 16:23:01 -0300

    fix initial style fixes, code errors and other

--- frontend/src/app/helpers/formatWallet.ts
+++ frontend/src/app/helpers/formatWallet.ts
@@ -1,6 +1,6 @@
-export function formatWallet(str: string) {
-  if (str === undefined) return "not connected";
-  const first5 = str.slice(0, 6);
-  const last5 = str.slice(-4);
+export function formatWallet(str: string | null) {
+  if (str === undefined || str === null) return "no wallet connected";
+  const first5 = str?.slice(0, 6);
+  const last5 = str?.slice(-4);
   return `${first5}...${last5}`;
 }
