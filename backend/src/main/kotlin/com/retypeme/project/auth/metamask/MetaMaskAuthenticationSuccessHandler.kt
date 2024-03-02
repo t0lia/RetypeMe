1f03f3e00c7e696446294f9bfbeea5ac749977d0
Anatoly <t0lia.gm@gmail.com>
2024-03-03 00:27:35 +0100

    remove redirects (#89)

--- backend/src/main/kotlin/com/retypeme/project/auth/metamask/MetaMaskAuthenticationSuccessHandler.kt
+++ backend/src/main/kotlin/com/retypeme/project/auth/metamask/MetaMaskAuthenticationSuccessHandler.kt
@@ -6,22 +6,19 @@ import jakarta.servlet.ServletException
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.Authentication
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import java.io.IOException
 
 
-class MetaMaskAuthenticationSuccessHandler(private val userRepository: UserRepository) :
-    SimpleUrlAuthenticationSuccessHandler() {
+class MetaMaskAuthenticationSuccessHandler(private val userRepository: UserRepository) : AuthenticationSuccessHandler {
 
     @Throws(ServletException::class, IOException::class)
     override fun onAuthenticationSuccess(
-        request: HttpServletRequest,
-        response: HttpServletResponse,
-        authentication: Authentication
+        request: HttpServletRequest, response: HttpServletResponse, authentication: Authentication
     ) {
-        super.onAuthenticationSuccess(request, response, authentication)
         val principal: MetaMaskUserDetails = authentication.principal as MetaMaskUserDetails
         val user: User = userRepository.getUser(principal.address)
         user.changeNonce()
+        response.status = HttpServletResponse.SC_OK
     }
 }
