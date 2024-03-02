b25e7a946abdbaa77f75815ff7087b62895edf9b
apozdniakov <t0lia.gm@gmail.com>
2024-03-02 01:35:17 +0100

    fix backend api routing

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,11 +2,7 @@
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
-  },
-  env: {
-    API_REST: process.env.API_REST,
-    API_WS: process.env.API_WS,
-  },
+  }
 };
 
 module.exports = nextConfig;
