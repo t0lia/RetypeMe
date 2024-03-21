4308fb55ea24878a401e82d2943d0ff40a44fe71
apozdniakov <t0lia.gm@gmail.com>
2024-03-21 19:42:32 +0100

    add encrypted properties

--- backend/src/test/kotlin/com/retypeme/backend/BackendApplicationTest.kt
+++ backend/src/test/kotlin/com/retypeme/backend/BackendApplicationTest.kt
@@ -7,8 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class BackendApplicationTest {
 
-    @Test
-    fun contextLoads() {
-    }
+//    @Test
+//    fun contextLoads() {
+//    }
 
 }
