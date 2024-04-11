6f41f375e06be722118d886ae816ebe60b2d8c84
mezger75 <9771804@gmail.com>
2024-04-11 17:23:30 -0300

    add chainId contants and deposit button title

--- frontend/src/app/constants/contract-constants.ts
+++ frontend/src/app/constants/contract-constants.ts
@@ -3,3 +3,7 @@
 
 // 0x13882 is Amoy 80002
 export const CHAIN_ID_AMOY = "0x13882";
+export const CHAIN_ID_BLAST_SEPOLIA = "0x13882";
+
+export const CHAIN_ID_AMOY_DECIMAL = 80002;
+export const CHAIN_ID_BLAST_SEPOLIA_DECIMAL = 168587773;
