aa7b79d4fcafd09e6a528d136a02cc74586030c5
Anatoly <t0lia.gm@gmail.com>
2024-04-25 23:40:27 +0200

    sc v2 (#119)

--- backend/src/main/resources/solidity/GamingContract.sol
+++ backend/src/main/resources/solidity/GamingContract.sol
@@ -1,24 +1,56 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.9;
 
-contract GamingContract {
+interface IGamingContract {
+    function deposit() external payable;
+
+    function withdraw(uint256) external;
+
+    function joinGame(uint256 _sessionId) external;
+
+    function getBalance() external view returns (uint256);
+
+    function isEnoughBalance() external view returns (bool);
+
+    function getMinDeposit() external view returns (uint256);
+
+    function endGame(uint256 _sessionId, address _winner) external;
+}
+
+// Main contract for RetypeMe
+contract GamingContract is IGamingContract {
+    // declare constant default players count for duel mode
+    uint256 public constant DUEL_PLAYERS_COUNT = 2;
+
     address public owner;
     uint256 public commissionRate;
     uint256 public fixedDepositAmount;
     uint256 public totalCommission;
 
-    struct Player {
-        address wallet;
-        uint256 depositAmount;
-        uint256 gameId;
+    enum GameStatus {
+        NOT_STARTED,
+        STARTED,
+        ENDED
     }
 
-    mapping(uint256 => Player[]) public gameSessions;
-    mapping(uint256 => address) public winners;
-    mapping(uint256 => uint256) public sessionWinnings;
+    struct GameSession {
+        uint256 depositAmount;
+        uint256 sessionId;
+        GameStatus status;
+        address[] players;
+        address winner;
+    }
+
+    mapping(address => uint256) public inGameUserBalances;
+    mapping(uint256 => GameSession) public gameSessions;
 
     event GameStarted(uint256 indexed sessionId);
-    event GameEnded(uint256 indexed sessionId, address winner, uint256 winnings);
+    event GameEnded(
+        uint256 indexed sessionId,
+        address winner,
+        uint256 winnings
+    );
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Not the owner");
@@ -28,66 +60,143 @@ contract GamingContract {
     constructor() {
         owner = msg.sender;
         commissionRate = 20;
-        fixedDepositAmount =  0.001 ether;
+        fixedDepositAmount = 0.001 ether;
     }
 
-    function deposit(uint256 sessionId) external payable {
-        require(msg.value == fixedDepositAmount, "Deposit amount must be equal fixed deposit amount");
+    function deposit() external payable {
+        inGameUserBalances[msg.sender] += msg.value;
+    }
 
-        Player memory player = Player({
-            wallet: msg.sender,
-            depositAmount: msg.value,
-            gameId: sessionId
-        });
+    function getBalance() external view returns (uint256) {
+        return inGameUserBalances[msg.sender];
+    }
 
-        gameSessions[sessionId].push(player);
+    function getMinDeposit() external view returns (uint256) {
+        // if there is no enough balance, show min amount to deposit to be able to play a game
+        return
+            fixedDepositAmount > inGameUserBalances[msg.sender]
+                ? fixedDepositAmount - inGameUserBalances[msg.sender]
+                : 0;
+    }
 
-        if (gameSessions[sessionId].length == 2) {
-            emit GameStarted(sessionId);
+    function isEnoughBalance() external view returns (bool) {
+        return inGameUserBalances[msg.sender] >= fixedDepositAmount;
+    }
+
+    // EXAMPLE OF SESSION ID: 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02d1
+    function joinGame(uint256 _sessionId) external {
+        // address sender = msg.sender; DELETE because of gas economy
+
+        // Check if the session already exists by checking a characteristic field, like sessionId
+        if (gameSessions[_sessionId].sessionId == 0) {
+            // This implies the session does not exist, so initialize it
+            gameSessions[_sessionId].sessionId = _sessionId;
+            gameSessions[_sessionId].depositAmount = 0; // Assuming it starts at 0 and increments with each deposit
+            gameSessions[_sessionId].status = GameStatus.NOT_STARTED;
+            gameSessions[_sessionId].players = new address[](0);
+            gameSessions[_sessionId].winner = address(0);
+        }
+
+        // Proceed with existing checks
+        require(
+            gameSessions[_sessionId].players.length == 0 ||
+            gameSessions[_sessionId].players[0] != msg.sender,
+            "Player already in the game"
+        );
+        require(
+            gameSessions[_sessionId].players.length < DUEL_PLAYERS_COUNT,
+            "Game session is full"
+        );
+        require(
+            gameSessions[_sessionId].status == GameStatus.NOT_STARTED,
+            "Game session already started"
+        );
+        require(
+            inGameUserBalances[msg.sender] >= fixedDepositAmount,
+            "Insufficient balance"
+        );
+
+        // Lock sender balance
+        inGameUserBalances[msg.sender] -= fixedDepositAmount;
+        gameSessions[_sessionId].depositAmount += fixedDepositAmount;
+
+        // Add the sender to the players list
+        gameSessions[_sessionId].players.push(msg.sender);
+
+        // If the session becomes full, start the game
+        if (gameSessions[_sessionId].players.length == DUEL_PLAYERS_COUNT) {
+            gameSessions[_sessionId].status = GameStatus.STARTED;
+            emit GameStarted(_sessionId);
         }
     }
 
     function endGame(uint256 _sessionId, address _winner) external onlyOwner {
-        require(gameSessions[_sessionId].length == 2, "Game not started");
-        require(_winner ==  gameSessions[_sessionId][0].wallet || _winner == gameSessions[_sessionId][1].wallet, "This address can't be the winner. It is not in the current game session.");
-
+        GameSession storage session = gameSessions[_sessionId];
+        // check if the game is started state
+        require(
+            session.status == GameStatus.STARTED,
+            "Game not started or already ended"
+        );
+        require(
+            session.players.length == DUEL_PLAYERS_COUNT,
+            "Wrong number of players in the game session"
+        );
+        require(
+            _winner == session.players[0] || _winner == session.players[1],
+            "This address can't be the winner. It is not in the current game session."
+        );
         // Logic to determine the winner and calculate winnings and commission
         // For simplicity, assuming winner is provided and winnings are total deposit amount minus commission
-
-        uint256 totalDeposit = gameSessions[_sessionId][0].depositAmount + gameSessions[_sessionId][1].depositAmount;
-        uint256 commission = (totalDeposit * commissionRate) / 100;
-        sessionWinnings[_sessionId] = totalDeposit - commission;
-        totalCommission+=commission;
-
-        winners[_sessionId] = _winner;
-
-        emit GameEnded(_sessionId, _winner, sessionWinnings[_sessionId]);
+        // ADDED new variable (winnings) to make gas cheaper
+        uint256 commission = (session.depositAmount * commissionRate) / 100;
+        uint256 winnings = session.depositAmount - commission;
+        // update winner balance
+        inGameUserBalances[_winner] += winnings;
+        totalCommission += commission;
+        // update winner
+        session.winner = _winner;
+        // update session status
+        session.status = GameStatus.ENDED;
+        emit GameEnded(_sessionId, _winner, winnings);
     }
 
-    function withdrawWinnings(uint256 _sessionId) external {
-        require(winners[_sessionId]==msg.sender, "Only the winner can withdraw winnings");
-
-        payable(msg.sender).transfer(sessionWinnings[_sessionId]);
+    // I think NO need to make two withdraw fns, because it will always get amount from FE after reading state. (there will be input
+    // with desirable amount and button "Max", it will check max balance and pass it to the withdraw fn)
+    function withdraw(uint256 amount) external {
+        address sender = msg.sender;
+        require(inGameUserBalances[sender] >= amount, "Insufficient balance");
+        inGameUserBalances[sender] -= amount;
+        payable(sender).transfer(amount);
     }
 
     function withdrawFundsFromContract() external onlyOwner {
-        // we are withdrawing all the balance
+        // we are withdrawing all the balance. will be deleted in future versions!
         require(address(this).balance > 0, "No funds to withdraw");
         payable(owner).transfer(address(this).balance);
     }
 
     function withdrawCommissionFromContract() external onlyOwner {
         // withdrawing only total commission
-        require(totalCommission>0, "No commision to withdraw at the moment");
-        payable(owner).transfer(totalCommission);
-        totalCommission = 0;
+        require(totalCommission > 0, "No commission to withdraw");
+        totalCommission = 0; // MODIFY FIRST
+        // payable(owner).transfer(totalCommission); // THEN TRANSFER
+        payable(owner).transfer(totalCommission); // THEN TRANSFER
     }
 
-    function changeCommissionRate(uint256 _newCommissionRate) public onlyOwner {
+    function changeCommissionRate(
+        uint256 _newCommissionRate
+    ) external onlyOwner {
+        require(
+            _newCommissionRate >= 0 && _newCommissionRate < 100,
+            "Enter valid number"
+        );
         commissionRate = _newCommissionRate;
     }
 
-    function changeFixedDepositAmount(uint256 _newFixedDepositAmount) public onlyOwner {
+    function changeFixedDepositAmount(
+        uint256 _newFixedDepositAmount
+    ) external onlyOwner {
+        require(_newFixedDepositAmount > 0, "Enter valid number");
         fixedDepositAmount = _newFixedDepositAmount;
     }
 }
