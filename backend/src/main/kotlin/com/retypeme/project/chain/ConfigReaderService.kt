4d62775bc01f0ced2ad6d51eb9ce6806ec5a8a7d
Anatoly <t0lia.gm@gmail.com>
2024-04-28 18:00:15 +0200

    Feature/ap/fix chain config (#125)
    * fix endGame call
    
    * fix chain configuration

--- backend/src/main/kotlin/com/retypeme/project/chain/ConfigReaderService.kt
+++ backend/src/main/kotlin/com/retypeme/project/chain/ConfigReaderService.kt
@@ -4,12 +4,34 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Service
+import java.io.File
 
 @Service
 class ConfigReaderService {
     fun readChainConfig(): ChainConfig {
         val objectMapper = ObjectMapper(YAMLFactory())
-        val resource = ClassPathResource("chains.yml").file
+        val resource: String = """chains:
+  - id: 534351
+    name: scroll-sepolia
+    rpc: "https://sepolia-rpc.scroll.io"
+    infura: false
+    contract: "0x078869dd68d019900098b5b1006951ea7b3f01f2"
+  - id: 80002
+    name: polygon-amoy
+    rpc: "https://polygon-amoy.infura.io/v3"
+    infura: true
+    contract: "0x993558c22ebe07c96e8f85d1ef4318c513abff0d"
+  - id: 31337
+    name: etherium-sepolia
+    rpc: "https://sepolia.infura.io/v3"
+    infura: true
+    contract: "0x0000000000000000000000000000000000000000"
+  - id: 168587773
+    name: blast-sepolia
+    rpc: "https://blast-sepolia.infura.io/v3"
+    infura: true
+    contract: "0xb3c33b58de859a5e06aff62c9d66319c256218da"
+        """.trimIndent()
         return objectMapper.readValue(resource, ChainConfig::class.java)
     }
 }
