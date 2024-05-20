1b0bb5d4d9ebfcf0db424a2e308df8f8497eeb6b
apozdniakov <t0lia.gm@gmail.com>
2024-05-20 11:05:46 +0200

    change leaderboard behaviour

--- backend/src/main/kotlin/com/retypeme/project/statistic/repository/LeaderboardRepository.kt
+++ backend/src/main/kotlin/com/retypeme/project/statistic/repository/LeaderboardRepository.kt
@@ -5,32 +5,29 @@ import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.core.ZSetOperations
 import org.springframework.stereotype.Repository
 
-
 interface LeaderboardRepository {
     fun addUserScore(userId: String, score: Double)
     fun getTopUsers(limit: Int): List<LeaderboardItemModel>
 }
 
 @Repository
-class LeaderboardRepositoryImpl(private val redisTemplate: RedisTemplate<String, LeaderboardItemModel>) :
-    LeaderboardRepository {
+class LeaderboardRepositoryImpl(private val redisTemplate: RedisTemplate<String, String>) : LeaderboardRepository {
 
     private val leaderboardKey = "leaderboard"
 
-    override fun addUserScore(userId: String, score: Double) {
-        val zSetOps: ZSetOperations<String, LeaderboardItemModel> = redisTemplate.opsForZSet()
-        val existingScore = zSetOps.score(leaderboardKey, userId)
+    private val zSetOps: ZSetOperations<String, String> by lazy {
+        redisTemplate.opsForZSet()
+    }
 
+    override fun addUserScore(userId: String, score: Double) {
+        val existingScore = zSetOps.score(leaderboardKey, userId)
         if (existingScore == null || score > existingScore) {
-            zSetOps.add(leaderboardKey, LeaderboardItemModel(userId, score), score)
+            zSetOps.add(leaderboardKey, userId, score)
         }
     }
 
     override fun getTopUsers(limit: Int): List<LeaderboardItemModel> {
-        val zSetOps: ZSetOperations<String, LeaderboardItemModel> = redisTemplate.opsForZSet()
-        val topUsers: MutableSet<ZSetOperations.TypedTuple<LeaderboardItemModel>> =
-            zSetOps.reverseRangeWithScores(leaderboardKey, 0, (limit - 1).toLong()) ?: return emptyList()
-
-        return topUsers.map { it.value!! }
+        val topUsers = zSetOps.reverseRangeWithScores(leaderboardKey, 0, (limit - 1).toLong())
+        return topUsers?.map { LeaderboardItemModel(it.value ?: "", it.score ?: 0.0) } ?: emptyList()
     }
 }
