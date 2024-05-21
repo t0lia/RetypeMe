b2adcbcf0c7745fce64e4dd3e7b7fa20d13ab456
mezger75 <9771804@gmail.com>
2024-05-20 21:42:56 -0300

    shorten userId name on leaderboard

--- frontend/src/app/helpers/format-wallet.ts
+++ frontend/src/app/helpers/format-wallet.ts
@@ -1,6 +1,6 @@
 export function formatWallet(str: string | null) {
   if (str === undefined || str === null) return "no wallet connected";
   const first5 = str?.slice(0, 6);
-  const last5 = str?.slice(-4);
-  return `${first5}...${last5}`;
+  const last4 = str?.slice(-4);
+  return `${first5}...${last4}`;
 }
