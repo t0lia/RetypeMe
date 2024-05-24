987b5765a624216692d048b6383805ee5d32dd47
mezger75 <9771804@gmail.com>
2024-05-24 17:13:40 -0300

    refactor header comp & add avatar from shadcn

--- frontend/src/app/components/game-page-header/gamePageHeader.tsx
+++ frontend/src/app/components/game-page-header/gamePageHeader.tsx
@@ -2,21 +2,25 @@ import React from "react";
 import ConnectButton from "../connect-button/connectButton";
 import ModeToggle from "@/app/components/ui/toggle-theme-changer";
 import Logo from "../logo/logo";
+import DropdownFaucetMenu from "../dropdown/dropdownFaucetMenu";
 
 interface IGamePageHeaderProps {
-  isButtonDisabled: boolean;
-  isGameEnded: boolean;
+  isButtonDisabled?: boolean;
+  isGameEnded?: boolean;
+  dropDownFaucetMenu?: boolean;
 }
 
 function GamePageHeader({
   isButtonDisabled,
   isGameEnded,
+  dropDownFaucetMenu,
 }: IGamePageHeaderProps) {
   return (
     <header className="flex h-16 justify-between items-center">
       <Logo />
       <div className="flex justify-end items-center p-4">
         <ModeToggle />
+        {dropDownFaucetMenu && <DropdownFaucetMenu />}
         <ConnectButton
           isButtonDisabled={isButtonDisabled}
           isGameEnded={isGameEnded}
