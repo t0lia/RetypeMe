1744be82d277cc92609c16e30106e3d09a91df03
mezger75 <9771804@gmail.com>
2024-05-24 21:59:42 -0300

    add user profile page and dropdown menu with user avatar

--- frontend/src/app/components/game-page-header/gamePageHeader.tsx
+++ frontend/src/app/components/game-page-header/gamePageHeader.tsx
@@ -3,6 +3,21 @@ import ConnectButton from "../connect-button/connectButton";
 import ModeToggle from "@/app/components/ui/toggle-theme-changer";
 import Logo from "../logo/logo";
 import DropdownFaucetMenu from "../dropdown/dropdownFaucetMenu";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { useSIWE } from "connectkit";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuPortal,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from "@/app/components/ui/dropdown-menu";
+import { LogOut, User } from "lucide-react";
+import Link from "next/link";
+import { useAccount } from "wagmi";
 
 interface IGamePageHeaderProps {
   isButtonDisabled?: boolean;
@@ -15,6 +30,8 @@ function GamePageHeader({
   isGameEnded,
   dropDownFaucetMenu,
 }: IGamePageHeaderProps) {
+  const { isSignedIn } = useSIWE();
+  const { address } = useAccount();
   return (
     <header className="flex h-16 justify-between items-center">
       <Logo />
@@ -25,6 +42,37 @@ function GamePageHeader({
           isButtonDisabled={isButtonDisabled}
           isGameEnded={isGameEnded}
         />
+        {isSignedIn && (
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar className="ml-2">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+                <AvatarFallback>RTM</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuContent>
+                <DropdownMenuGroup>
+                  <DropdownMenuLabel>My account</DropdownMenuLabel>
+                  <Link href={`/user/${address}`} target="_blank">
+                    <DropdownMenuItem className="cursor-pointer">
+                      <User className="mr-2 h-4 w-4" />
+                      <span>Profile</span>
+                    </DropdownMenuItem>
+                  </Link>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem className="cursor-pointer">
+                    <LogOut className="mr-2 h-4 w-4" />
+                    <span>Log out</span>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenuPortal>
+          </DropdownMenu>
+        )}
       </div>
     </header>
   );
---
+++ frontend/src/app/user/[id]/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Footer from "@/app/components/footer/footer";
+import GamePageHeader from "@/app/components/game-page-header/gamePageHeader";
+
+export default function UserProfile() {
+  return (
+    <>
+      <GamePageHeader></GamePageHeader>
+      <main className="h-screen flex flex-col relative">main</main>
+      <Footer />
+    </>
+  );
+}
