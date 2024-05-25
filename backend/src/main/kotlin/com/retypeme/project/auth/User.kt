4d5d1c42b295f397afb4b9910df1bd05bbf4d77c
apozdniakov <t0lia.gm@gmail.com>
2024-05-25 22:37:29 +0200

    move session and user management to redis

--- backend/src/main/kotlin/com/retypeme/project/auth/User.kt
+++ backend/src/main/kotlin/com/retypeme/project/auth/User.kt
@@ -1,11 +1,12 @@
 package com.retypeme.project.auth
 
 import com.moonstoneid.siwe.util.Utils.generateNonce
+import java.io.Serializable
 
 data class User(
     val address: String,
     var nonce: String = generateNonce()
-) {
+) : Serializable {
     fun changeNonce() {
         nonce = generateNonce()
     }
