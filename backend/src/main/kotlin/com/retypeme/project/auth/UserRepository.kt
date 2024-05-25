353228840f8879ad41e25ea291d3ecf6b35c9d75
apozdniakov <t0lia.gm@gmail.com>
2024-05-25 22:37:29 +0200

    move session and user management to redis

--- backend/src/main/kotlin/com/retypeme/project/auth/UserRepository.kt
+++ backend/src/main/kotlin/com/retypeme/project/auth/UserRepository.kt
@@ -1,12 +1,18 @@
 package com.retypeme.project.auth
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.redis.core.HashOperations
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Repository
-import java.util.concurrent.ConcurrentHashMap
-
 
 @Repository
-class UserRepository {
-    private val users: MutableMap<String, User> = ConcurrentHashMap()
+class UserRepository(@Autowired val redisTemplate: RedisTemplate<String, User>) {
 
-    fun getUser(id: String): User = users.computeIfAbsent(id) { User(it) }
+    private val hashOps: HashOperations<String, String, User> by lazy {
+        redisTemplate.opsForHash()
+    }
+
+    fun getUser(id: String): User {
+        return hashOps.get("users", id) ?: User(id).also { hashOps.put("users", id, it) }
+    }
 }
--- backend/src/main/kotlin/com/retypeme/project/control/Session.kt
+++ backend/src/main/kotlin/com/retypeme/project/control/Session.kt
@@ -1,3 +1,5 @@
 package com.retypeme.project.control
 
-class Session(val id: String, val chain: Int, var players: Int)
+import java.io.Serializable
+
+data class Session(val id: String, val chain: Int, var players: Int) : Serializable
