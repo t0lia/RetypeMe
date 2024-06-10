b1557b4b0dff984ab85742b9f49f343741d568c6
mezger75 <110202847+mezger75@users.noreply.github.com>
2024-06-10 20:57:33 +0300

    Feature/ik/UI fixes (#181)
    * add mono font to leaderboard & to main game text and some fixes
    
    * fix formatting total rewards fn
    
    * update readme file with opbnb contract
    
    * fix faucet links
    
    * fix chains

--- frontend/src/app/components/dropdown/dropdownFaucetMenu.tsx
+++ frontend/src/app/components/dropdown/dropdownFaucetMenu.tsx
@@ -52,7 +52,7 @@ function DropdownFaucetMenu() {
 
           <DropdownMenuSeparator className="h-[1px] bg-gray-300 m-[4px]" /> */}
 
-          {/* <DropdownMenuGroup>
+          <DropdownMenuGroup>
             <DropdownMenuLabel className="text-sm px-3 py-1">
               BNB Faucet & Bridge
             </DropdownMenuLabel>
@@ -72,7 +72,7 @@ function DropdownFaucetMenu() {
                 Bridge to opBNB
               </DropdownMenuItem>
             </Link>
-          </DropdownMenuGroup> */}
+          </DropdownMenuGroup>
 
           {/* <DropdownMenuSeparator className="h-[1px] bg-gray-300 m-[4px]" /> */}
 
@@ -105,7 +105,7 @@ function DropdownFaucetMenu() {
 
           <DropdownMenuSeparator className="h-[1px] bg-gray-300 m-[4px]" /> */}
 
-          <DropdownMenuGroup>
+          {/* <DropdownMenuGroup>
             <DropdownMenuLabel className="text-sm px-3 py-1">
               ETH Blast Sepolia
             </DropdownMenuLabel>
@@ -125,7 +125,7 @@ function DropdownFaucetMenu() {
                 QuickNode faucet
               </DropdownMenuItem>
             </Link>
-          </DropdownMenuGroup>
+          </DropdownMenuGroup> */}
         </DropdownMenuContent>
       </DropdownMenuPortal>
     </DropdownMenu>
