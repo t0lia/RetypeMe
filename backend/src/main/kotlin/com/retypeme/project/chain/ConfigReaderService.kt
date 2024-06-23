f662bdad654da314fee7224e21aadc6b2bcd8a26
apozdniakov <t0lia.gm@gmail.com>
2024-06-23 16:31:52 +0200

    update base config

--- backend/src/main/kotlin/com/retypeme/project/chain/ConfigReaderService.kt
+++ backend/src/main/kotlin/com/retypeme/project/chain/ConfigReaderService.kt
@@ -14,7 +14,17 @@ class ConfigReaderService {
     name: blast-sepolia
     rpc: "https://blast-sepolia.infura.io/v3"
     contract: "0xb3c33b58de859a5e06aff62c9d66319c256218da"
-   
+    
+  - id: 84532
+    name: base-sepolia
+    rpc: "https://sepolia.base.org"
+    contract: "0xb4eb30e7f583d788a1611f4b7022bdda4bd4af81"
+ 
+  - id: 5611
+    name: opbnb-testnet-rpc
+    rpc: "https://opbnb-testnet-rpc.bnbchain.org"
+    contract: "0x247e2bee76ec31c1a4caaf06600a80ffd6774dd6"
+    
 """.trimIndent()
 
     fun readChainConfig(): ChainConfig {
