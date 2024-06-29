a6887e064f339cffd3140b7178758a0b00f7c7a3
Anatolii <t0lia.gm@gmail.com>
2024-06-29 09:27:12 +0200

    update chain config (#186)

--- backend/src/main/kotlin/com/retypeme/project/chain/ConfigReaderService.kt
+++ backend/src/main/kotlin/com/retypeme/project/chain/ConfigReaderService.kt
@@ -9,21 +9,11 @@ class ConfigReaderService {
     val config: String =
         """
   chains:
- 
-  - id: 168587773
-    name: blast-sepolia
-    rpc: "https://blast-sepolia.infura.io/v3"
-    contract: "0xb3c33b58de859a5e06aff62c9d66319c256218da"
-    
+
   - id: 84532
     name: base-sepolia
     rpc: "https://sepolia.base.org"
     contract: "0xb4eb30e7f583d788a1611f4b7022bdda4bd4af81"
- 
-  - id: 5611
-    name: opbnb-testnet-rpc
-    rpc: "https://opbnb-testnet-rpc.bnbchain.org"
-    contract: "0x247e2bee76ec31c1a4caaf06600a80ffd6774dd6"
     
 """.trimIndent()
 
--- frontend/src/app/store/configStore.ts
+++ frontend/src/app/store/configStore.ts
@@ -229,24 +229,24 @@ const chains: ChainConfig[] = [
   //   "rpc": `https://scroll-sepolia.core.chainstack.com/c02e3ccda5dd045b98b943c1ab129544`,
   //   "contract": "0x078869dd68d019900098b5b1006951ea7b3f01f2"
   // },
-  {
-    id: 5611,
-    name: "opbnb-testnet",
-    rpc: "https://opbnb-testnet-rpc.publicnode.com",
-    contract: "0x247e2bee76ec31c1a4caaf06600a80ffd6774dd6",
-  },
+  // {
+  //   id: 5611,
+  //   name: "opbnb-testnet",
+  //   rpc: "https://opbnb-testnet-rpc.publicnode.com",
+  //   contract: "0x247e2bee76ec31c1a4caaf06600a80ffd6774dd6",
+  // },
   {
     id: 84532,
     name: "base-sepolia",
     rpc: "https://sepolia.base.org",
     contract: "0xb4eb30e7f583d788a1611f4b7022bdda4bd4af81",
   },
-  {
-    id: 168587773,
-    name: "blast-sepolia",
-    rpc: `https://blast-sepolia.infura.io/v3/91f259e2739445678d072c85496b47b7`,
-    contract: "0xb3c33b58de859a5e06aff62c9d66319c256218da",
-  },
+  // {
+  //   id: 168587773,
+  //   name: "blast-sepolia",
+  //   rpc: `https://blast-sepolia.infura.io/v3/91f259e2739445678d072c85496b47b7`,
+  //   contract: "0xb3c33b58de859a5e06aff62c9d66319c256218da",
+  // },
   // {
   //   "id": 80002,
   //   "name": "polygon-amoy",
