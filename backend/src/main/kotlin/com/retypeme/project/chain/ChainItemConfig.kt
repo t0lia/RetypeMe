305c8e79c85e2fdf4d0bccf697f9cc88a6f3510a
apozdniakov <t0lia.gm@gmail.com>
2024-07-07 18:27:19 +0200

    migrate config to separate repo

--- backend/src/main/kotlin/com/retypeme/project/chain/ChainItemConfig.kt
+++ backend/src/main/kotlin/com/retypeme/project/chain/ChainItemConfig.kt
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class ChainItemConfig @JsonCreator constructor(
     @JsonProperty("id") val id: Int,
     @JsonProperty("name") val name: String,
+    @JsonProperty("infura") val infura: Boolean,
     @JsonProperty("rpc") val rpc: String,
     @JsonProperty("contract") val contract: String
+
 )
