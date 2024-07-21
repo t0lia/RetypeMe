3b6cf5fd9fefb26a9f11b6f68d948d6244b67a3a
mezger75 <9771804@gmail.com>
2024-07-21 22:29:48 +0300

    add types to configStore

--- frontend/src/app/store/configStore.ts
+++ frontend/src/app/store/configStore.ts
@@ -1,17 +1,19 @@
-import {create} from "zustand";
+import { create } from "zustand";
 import ApiDomainService from "@/app/api/api-domain-service";
+import { Abi } from "viem";
 
 export interface ConfigState {
-  contractConfig: ContractConfig; // Define a more specific type based on your configuration
-  setConractConfig: (config: any) => void;
+  contractConfig: ContractConfig;
+  setConractConfig: (config: ContractConfig) => void;
   fetchContractConfig: () => Promise<ContractConfig>;
   wagmiConfig: any;
   setWagmiConfig: (config: any) => void;
 }
 
+0;
 export interface ContractConfig {
   chains: ChainConfig[];
-  abi: any[];
+  abi: Abi[];
 }
 
 export interface ChainConfig {
@@ -21,23 +23,22 @@ export interface ChainConfig {
   contract: string;
 }
 
-
-const abi: any[] = [];
+const abi: Abi[] = [];
 
 const chains: ChainConfig[] = [];
 
 export const useConfigStore = create<ConfigState>((set) => ({
-  contractConfig: {chains, abi},
+  contractConfig: { chains, abi },
   wagmiConfig: {},
-  setConractConfig: (contractConfig) => set({contractConfig}),
-  setWagmiConfig: (wagmiConfig) => set({wagmiConfig}),
+  setConractConfig: (contractConfig) => set({ contractConfig }),
+  setWagmiConfig: (wagmiConfig) => set({ wagmiConfig }),
   fetchContractConfig: async () => {
     const apiUrl = new ApiDomainService().getRestUrl();
     return fetch(`${apiUrl}/contract/config`)
       .then((response) => response.json())
       .then((data) => {
-        set({contractConfig: data});
+        set({ contractConfig: data });
         return data;
       });
-  }
+  },
 }));
